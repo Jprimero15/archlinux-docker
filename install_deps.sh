e5c5cafd273f7b2cb70bfefceb70c5700e1042b8
Jprimero15 <jprimero15@aospa.co>
2024-02-20 15:56:02 +0800

    cleanups

--- install_deps.sh
+++ install_deps.sh
@@ -9,11 +9,11 @@ pacman -Syyu --noconfirm
 
 # Install basic packages
 pacman -S --noconfirm \
-                 wget nano asp kmod libelf pahole xmlto \          
-                 cmake svn lzip git make patchelf zip \
+                 wget nano kmod libelf pahole xmlto \
+                 svn lzip git make patchelf zip \
                  inetutils base-devel neofetch ncurses \
-                 clang bc ccache multilib-devel glibc z3 \
-                 sudo bison cmake flex libelf cpio \
+                 clang bc ccache glibc z3 sudo \
+                 bison cmake flex libelf cpio \
                  ninja openssl python3 dpkg unzip \
                  aarch64-linux-gnu-gcc arm-none-eabi-binutils
 
@@ -22,9 +22,3 @@ export PATH=/usr/bin/core_perl:$PATH
 
 # Create a symlink for z3
 ln -s /usr/lib/libz3.so /usr/lib/libz3.so.4
-
-# Setup locale
-echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
-echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
-echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
-sudo locale-gen en_US.UTF-8
